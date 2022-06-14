5bad0e191d328f9d08f8a2f1714872106426e1a2
LuizEduardoGalvao <105395355+LuizEduardoGalvao@users.noreply.github.com>
2022-06-13 21:36:50 -0300

    save

---
+++ Cloth Storee/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Cloth_Storee.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cloth_Storee.Controllers
+{
+    public class HomeController : Controller
+    {      
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult bone()
+        {
+            return View();
+        }
+        public IActionResult calca()
+        {
+            return View();
+        }
+        public IActionResult camisas()
+        {
+            return View();
+        }
+        public IActionResult moletom()
+        {
+            return View();
+        }
+        public IActionResult tenis()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult cadastro()
+        {
+            return View();
+        }
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cloth Storee/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cloth_Storee.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
